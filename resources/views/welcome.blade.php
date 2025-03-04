0bf178418bf32c3cffaf51afc769316bd291a118
IchBin <boyfromhell86@gmail.com>
2025-03-04 22:16:01 +0000

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.base>
-        <flux:heading size="xl" level="1">Good afternoon, Olivia</flux:heading>
+{{--        <flux:heading size="xl" level="1">Good afternoon, Olivia</flux:heading>--}}
 
-        <flux:subheading size="lg" class="mb-6">Here's what's new today</flux:subheading>
+{{--        <flux:subheading size="lg" class="mb-6">Here's what's new today</flux:subheading>--}}
 
-        <flux:separator variant="subtle" />
+{{--        <flux:separator variant="subtle" />--}}
 
 </x-layouts.base>
