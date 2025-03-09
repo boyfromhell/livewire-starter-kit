700a230497ce209783484ede721d33200e909cda
IchBin <boyfromhell86@gmail.com>
2025-03-09 14:58:17 +0000

    some fix

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -44,21 +44,12 @@
 {{--                label="Documentation"--}}
 {{--            />--}}
 {{--        </flux:tooltip>--}}
-
-        <flux:dropdown x-data align="end">
-            <flux:button variant="subtle" square class="group" aria-label="Preferred color scheme">
-                <flux:icon.sun x-show="$flux.appearance === 'light'" variant="mini" class="text-zinc-500 dark:text-white" />
-                <flux:icon.moon x-show="$flux.appearance === 'dark'" variant="mini" class="text-zinc-500 dark:text-white" />
-                <flux:icon.moon x-show="$flux.appearance === 'system' && $flux.dark" variant="mini" />
-                <flux:icon.sun x-show="$flux.appearance === 'system' && ! $flux.dark" variant="mini" />
-            </flux:button>
-
-            <flux:menu>
-                <flux:menu.item icon="sun" x-on:click="$flux.appearance = 'light'">Light</flux:menu.item>
-                <flux:menu.item icon="moon" x-on:click="$flux.appearance = 'dark'">Dark</flux:menu.item>
-                <flux:menu.item icon="computer-desktop" x-on:click="$flux.appearance = 'system'">System</flux:menu.item>
-            </flux:menu>
-        </flux:dropdown>
+        <flux:button variant="subtle" square x-data x-on:click="$flux.dark = ! $flux.dark" aria-label="Preferred color scheme">
+            <flux:icon.sun x-show="$flux.appearance === 'light'" variant="mini" class="text-zinc-500 dark:text-white" />
+            <flux:icon.moon x-show="$flux.appearance === 'dark'" variant="mini" class="text-zinc-500 dark:text-white" />
+            <flux:icon.moon x-show="$flux.appearance === 'system' && $flux.dark" variant="mini" />
+            <flux:icon.sun x-show="$flux.appearance === 'system' && ! $flux.dark" variant="mini" />
+        </flux:button>
 
     </flux:navbar>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="p-4 sm:p-6 dark:bg-zinc-800 mt-auto">
+<footer class="dark:bg-zinc-800 mt-auto">
     <div class="mx-auto max-w-screen-xl">
         <hr class="my-6 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
             <span class="text-sm text-zinc-500 sm:text-center dark:text-zinc-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Flowbite™</a>. All Rights Reserved.
             </span>
-            <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            <div class="flex mt-4 space-x-2 sm:justify-center sm:mt-0">
                 <a href="#" class="text-zinc-500 hover:text-zinc-900 dark:hover:text-white">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                 </a>
